598e86e0d6fa009283fbd1c0bd41025266682ac9
AlexanderAlatorreGovea <alexander_alatorre1993@hotmail.com>
2021-07-02 22:41:01 -0600

    changed index file

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const init = () => {
+    const container = document.querySelector(".jumbo-slider");
+    const slides = document.querySelector(".jumbo-slider__slide");
+    const circles = document.querySelector(".jumbo-slider__circle");
+    const links = document.querySelector(".jumbo-slider__links");
+
+    let current = 1;  
+    let time = 6000;
+
+    slides.classList.add("jumbo-slider__slide--active")
+
+    links[current -1].classList.add("")
+}
